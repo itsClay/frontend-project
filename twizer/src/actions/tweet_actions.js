f4b063c36a788412d08a97ae5fd5fe1e48aafe07
Clay Shaw <Claydshaw@gmail.com>
2018-03-13 08:28:16 -0700

    Input Errors Handling action

--- twizer/src/actions/tweet_actions.js
+++ twizer/src/actions/tweet_actions.js
@@ -1,15 +1,21 @@
 import * as APIUtil from '../utils/tweets_api_utils'
 
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
+export const RECEIVE_TWEET_ERRORS = 'RECEIVE_TWEET_ERRORS'
 
 const receiveTweets = tweets => ({
   type: RECEIVE_TWEETS,
   tweets
 })
 
+const receiveTweetErrors = errors => ({
+  type: RECEIVE_TWEET_ERRORS,
+  errors
+})
+
 export const fetchTweets = () => dispatch => {
   return APIUtil.fetchTweets().then(
     tweets => dispatch(receiveTweets(tweets)),
     errors => dispatch(receiveTweetErrors(errors.responseJSON))
-  )
+  );
 }
