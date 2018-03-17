ca71ecd4420720be16260e7548ba515632dc3a99
Clay Shaw <Claydshaw@gmail.com>
2018-03-17 15:18:19 -0700

    Setup server

---
+++ twizer/backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 8080
+
+// runs our app through our routes and setup with just an object for DB later
+require('./api/index')(app, {})
+
+// add middlewares
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// setup routes for api and for static files
+// app.use('/', express.static(path.join(__dirname, '../public')))
+
+// instantiate listener
+app.listen(port, () => { console.log('Live on port: ', port) })
