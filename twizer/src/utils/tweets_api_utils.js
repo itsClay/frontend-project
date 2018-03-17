4f01b737a8d1f4325dded275cd2e3fe2fd308d8b
Clay Shaw <Claydshaw@gmail.com>
2018-03-17 15:23:47 -0700

    Clean up api call now that handled on backend

--- twizer/src/utils/tweets_api_utils.js
+++ twizer/src/utils/tweets_api_utils.js
@@ -1,24 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
 
-import { CONSUMER_KEY, CONSUMER_SECRET } from '../secret_keys';
-
-const username1 = 'Trizic'
-const username2 = 'laughingsquid'
-const username3 = 'techcrunch'
-
-const api = axios.create({
-  url: 'https://api.twitter.com/1.1/statuses/user_timeline.json',
-  // baseURL: 'https://api.twitter.com/1.1/',
-  headers: {
-    Authorization: `oauth_consumer_key=${CONSUMER_KEY}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1520898724&oauth_nonce=oIp3iW&oauth_version=1.0&oauth_signature=a93pBAPkgI63piwEo8DrxXi1gGs%3D`
-  },
-  params: { screen_name: `${username1}`, count: 30 },
-  withCredentials: true
-})
-
-export const fetchTweets = () => {
-  api.get().then(resp => {
-    console.log(resp)
-    return resp.data
+export const fetchTweets = (screen_name = 'Trizic') => {
+  return axios({
+    method: 'GET',
+    url: `/user_timeline/${screen_name}`,
+    withCredentials: true,
   })
 }
