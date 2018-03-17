749b042b9247da48d4677503da72b45ddd93789b
Clay Shaw <Claydshaw@gmail.com>
2018-03-17 15:22:21 -0700

    Setup response data for adding additional tweets by nesting within a key of id

--- twizer/src/reducers/tweets_reducer.js
+++ twizer/src/reducers/tweets_reducer.js
@@ -1,12 +1,18 @@
 import { RECEIVE_TWEETS } from '../actions/tweet_actions'
+import { merge } from 'lodash'
 
 const TweetsReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch(action.type) {
     case RECEIVE_TWEETS:
-      return Object.assign({}, state, action.tweets)
+      const newTweets = {}
+      action.tweets.forEach(tweet => {
+        newTweets[tweet.id] = tweet
+      })
+      return merge({}, state, newTweets)
       // return Object.assign({}, state, action.tweets)
+
       default:
         return state;
   }
