71d86834aec5d6e22d280277c090b5dcd1493de8
Clay Shaw <Claydshaw@gmail.com>
2018-03-18 13:03:52 -0700

    add loading features and setup

--- twizer/src/components/tweets/tweets_index.jsx
+++ twizer/src/components/tweets/tweets_index.jsx
@@ -1,18 +1,63 @@
-import React from 'react';
+import React from 'react'
+import Moment from 'moment'
+
+import './tweets_index.css'
+import TweetCard from './tweet_card'
 
 class TweetsIndex extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+      isLoading: true
+    }
+    this.formateDate = this.formatDate.bind(this);
+  }
 
   componentWillMount() {
-    this.props.fetchTweets('Trizic');
-    this.props.fetchTweets('laughingsquid')
-    this.props.fetchTweets('techcrunch')
-    // console.log(this.props)
+    // fetch the different tweets we need with some loading swapping
+    (() => {
+      this.setState({isLoading: true})
+      this.props.fetchTweets('Trizic');
+      this.props.fetchTweets('laughingsquid')
+      this.props.fetchTweets('techcrunch')
+      this.setState({isLoading: false})
+    })()
+  }
+
+  formatDate(twitterDate) {
+    return Moment(twitterDate).fromNow()
   }
 
   render() {
+    if(this.state.isLoading) {
+      return (<h2>...Loading</h2>)
+    }
+    const tweets = Object.values(this.props.tweets)
+
     return (
-      <h2> Tweets index
-      </h2>
+
+      <div>
+        <div className="TweetsIndex__Header">Tweets index</div>
+        <div className="TweetsIndex">
+          {
+            tweets.map((tweet, idx) => (
+              <TweetCard 
+                key={idx}
+                content={tweet.text} 
+                created_at={this.formatDate(tweet.created_at)} 
+                link={
+                  // not every tweet seems to come with a link to its tweet (perhaps if its deleted?)
+                  // design decision to just send the user to the home page for now if there is none
+                  tweet.entities.urls[0] === undefined ? "https://twitter/com" : tweet.entities.urls[0].url 
+                } 
+                username={tweet.user.screen_name}
+              />
+            ))
+          }
+        </div>
+      </div>
+
     )
   }
 
