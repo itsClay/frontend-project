a57a06ab4c13ef705eb46fd8c1b2db7153081ec4
Clay Shaw <Claydshaw@gmail.com>
2018-03-18 22:59:14 -0700

    attempt: doesn't work but trying work around for webpack pre processing

--- twizer/src/components/root.jsx
+++ twizer/src/components/root.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import App from '../App'
 import { Provider } from 'react-redux';
 
+let settings = JSON.parse(window.localStorage.getItem('settings'))
+if(settings.theme === 'nightMode') {
+  //...
+  require('./night_mode.css')
+}
 // this is a little overkill here until we try to set up routes
 const Root = ({ store }) => (
   <Provider store={store}>
